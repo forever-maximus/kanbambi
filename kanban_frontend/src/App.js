20823be9879564d3de0c7674d166ddf4a50cfddd
Max Brereton <max.brereton@gmail.com>
2018-12-06 11:36:37 +1100

    Added basic navbar.

--- kanban_frontend/src/App.js
+++ kanban_frontend/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './containers/Home';
 import Board from './containers/Board';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route path="/" exact component={Home} />
-          <Route path="/boards/:id" component={Board} />
+          <div className='app-navbar'>
+            <NavBar />
+          </div>
+          <div className='app-main'>
+            <Route path="/" exact component={Home} />
+            <Route path="/boards/:id" component={Board} />
+          </div>
         </div>
       </Router>
     );
---
+++ kanban_frontend/src/components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavBar = () => (
+  <h2>Kanbambi</h2>
+);
+
+export default NavBar;
