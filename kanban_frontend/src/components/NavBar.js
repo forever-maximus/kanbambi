b506741a84546688bc4d67d838954c640b344304
Max Brereton <max.brereton@gmail.com>
2018-12-07 12:42:51 +1100

    Add home navigation to navbar.

--- kanban_frontend/src/components/NavBar.js
+++ kanban_frontend/src/components/NavBar.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import './NavBar.css';
 
 const NavBar = () => (
-  <h2>Kanbambi</h2>
+  <div className='nav-wrapper'>
+    <Link className='home' to='/'><Button icon='home' /></Link>
+    <Link className='title' to='/'><h2>Kanbambi</h2></Link>
+  </div>
 );
 
 export default NavBar;
